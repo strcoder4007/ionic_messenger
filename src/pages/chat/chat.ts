708ad59077ccb617f54dd3f65fe4ec7d0c3e2308
Shubham Singh <mynameshubham@gmail.com>
2017-11-19 20:52:32 +0400

    options on alert added

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,25 +1,89 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Platform, ActionSheetController } from 'ionic-angular';
 
-/**
- * Generated class for the ChatPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
-  selector: 'page-chat',
-  templateUrl: 'chat.html',
+    selector: 'page-chat',
+    templateUrl: 'chat.html',
 })
 export class ChatPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public platform: Platform, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public actionsheetCtrl: ActionSheetController) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ChatPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ChatPage');
+    }
+
+    showConfirm() {
+        let confirm = this.alertCtrl.create({
+            title: 'Go Private?',
+            message: 'User 2 has asked you to go private',
+            buttons: [
+                {
+                    text: 'Disagree',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'Agree',
+                    handler: () => {
+                        console.log('Agree clicked');
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
+    openMenu() {
+        let actionSheet = this.actionsheetCtrl.create({
+            title: 'Albums',
+            cssClass: 'action-sheets-basic-page',
+            buttons: [
+                {
+                    text: 'See profile',
+                    role: 'destructive',
+                    icon: !this.platform.is('ios') ? 'person' : null,
+                    handler: () => {
+                        console.log('Delete clicked');
+                    }
+                },
+                {
+                    text: 'Go private',
+                    icon: !this.platform.is('ios') ? 'lock' : null,
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                },
+                {
+                    text: 'Time a message',
+                    icon: !this.platform.is('ios') ? 'time' : null,
+                    handler: () => {
+                        console.log('Play clicked');
+                    }
+                },
+                {
+                    text: 'Settings',
+                    icon: !this.platform.is('ios') ? 'settings' : null,
+                    handler: () => {
+                        console.log('Favorite clicked');
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel', // will always sort to be on the bottom
+                    icon: !this.platform.is('ios') ? 'close' : null,
+                    handler: () => {
+                        console.log('Cancel clicked');
+                    }
+                }
+            ]
+        });
+        actionSheet.present();
+    }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import{ ChatPage } from '../chat/chat';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
-  }
+    }
 
-  gotoChatPage() {
-    this.navCtrl.push(ChatPage);    
-  }
+    gotoChatPage() {
+        this.navCtrl.push(ChatPage);    
+    }
 
 }
